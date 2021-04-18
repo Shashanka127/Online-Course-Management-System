7e44e547223b9c6c3fb29b17eda79fc283fc1577
Shashanka Venkatesh <shashankavenkatesh127@gmail.com>
2021-04-18 17:08:01 +0530

    Fixed Welcome Page

--- scube-online-course-management-system/src/pages/Welcome.js
+++ scube-online-course-management-system/src/pages/Welcome.js
@@ -5,7 +5,7 @@ export default function Welcome() {
   return (
     <div>
       <div className="mx-auto">
-        <div className="relative z-10 pt-10 pb-44 bg-white lg:max-w-2xl h-full">
+        <div className="relative z-10 pt-10 pb-5 bg-white lg:max-w-2xl h-screen">
           <svg
             className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
             fill="currentColor"
@@ -16,20 +16,20 @@ export default function Welcome() {
             <polygon points="50,0 100,0 50,100 0,100" />
           </svg>
 
-          <main className="mx-auto max-w-5xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 h-full">
+          <main className="mx-auto max-w-5xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 ">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-5xl tracking-tight font-extrabold text-indigo-900 sm:text-5xl md:text-6xl">
                 <span className="block xl:inline">SCUBE<br/></span>{' '}
-                <span className="block text-blue-600 xl:inline">Online Course<br/>Management<br/>System</span>
+                <span className="block text-blue-600 xl:inline">Online Course<br/>Management System</span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                The best thing since sliced bread.
+                The best damn thing since sliced bread.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                 <Link
                   to="/login"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-700 hover:bg-indigo-800 md:py-4 md:text-lg md:px-10"
                 >
                   Log in
                 </Link>
@@ -37,7 +37,7 @@ export default function Welcome() {
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                 <Link
                   to="/register"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-300 hover:bg-indigo-500 md:py-4 md:text-lg md:px-10"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 md:py-4 md:text-lg md:px-10"
                 >
                   Register
                 </Link>
