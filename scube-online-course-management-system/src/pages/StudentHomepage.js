188ad2b9f837a64de43b010a281f0fd813c9a12b
Shashanka Venkatesh <shashankavenkatesh127@gmail.com>
2021-04-18 18:39:29 +0530

    Renamed Student Homepage Component

--- scube-online-course-management-system/src/pages/StudentHomepage.js
+++ scube-online-course-management-system/src/pages/StudentHomepage.js
@@ -1,15 +1,18 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import Profile from '../components/Profile'
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+export default function StudentHomepage() {
+  let history = useHistory();
 
-export default function Homepage() {
+  const signOutHandler = () => {
+    localStorage.clear();
+    history.push('/');
+    window.location.reload();
+  }
+
   return (
     <div>
       <Disclosure as="nav" className="bg-gray-800">
@@ -27,7 +30,7 @@ export default function Homepage() {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-10 flex items-baseline space-x-4">
-                    <Link to='/home' className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                    <Link to='/studenthome' className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
                       Your Profile
                     </Link>
                     <Link to='/enrolledCourses' className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
@@ -41,7 +44,7 @@ export default function Homepage() {
                 </div>
                 <div className="hidden md:block">
                   <div className="ml-4 flex items-center md:ml-6">
-                    <button className="bg-red-500 p-1 rounded-md text-white hover:text-red-500 hover:bg-white px-5 py-2 m-5 text-sm font-medium">
+                    <button onClick={() => signOutHandler()} className="bg-red-500 p-1 rounded-md text-white hover:text-red-500 hover:bg-white px-5 py-2 m-5 text-sm font-medium">
                       Sign Out
                     </button>
                     <div className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
@@ -69,7 +72,7 @@ export default function Homepage() {
 
             <Disclosure.Panel className="md:hidden">
               <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <Link to='/home' className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
+                <Link to='/studenthome' className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
                   Your Profile
                 </Link>
                 <Link to='/enrolledCourses' className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
@@ -92,7 +95,7 @@ export default function Homepage() {
                     <div className="text-base font-medium leading-none text-white">Tom Cook</div>
                     <div className="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
                   </div>
-                  <button className="ml-auto bg-red-500 p-1 rounded-md text-white hover:text-red-500 hover:bg-white px-5 py-2 m-5 text-sm font-medium">
+                  <button onClick={() => signOutHandler()} className="ml-auto bg-red-500 p-1 rounded-md text-white hover:text-red-500 hover:bg-white px-5 py-2 m-5 text-sm font-medium">
                     Sign Out
                   </button>
                 </div>
