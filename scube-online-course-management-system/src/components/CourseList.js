3e88dd624b5941de4cea558a7c70eeb4c2b5002f
Shashanka Venkatesh <shashankavenkatesh127@gmail.com>
2021-04-18 20:39:34 +0530

    Added Enrollment Handler (not working)

--- scube-online-course-management-system/src/components/CourseList.js
+++ scube-online-course-management-system/src/components/CourseList.js
@@ -1,9 +1,16 @@
-import { useEffect } from 'react'
-
-const courses = [
-]
+import { Fragment, useRef, useState, useEffect } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationIcon } from '@heroicons/react/outline'
+import { useHistory } from 'react-router';
 
 export default function CourseList( { username } ) {
+  let history = useHistory();
+  const [courses, setCourses] = useState([]);
+  const [enrollAlert, setEnrollAlert] = useState(false);
+  const [chosenCourse, setChosenCourse] = useState("");
+
+  const cancelButtonRef = useRef()
+
   useEffect(() => {
     if (username === "none") {
       fetch('/api/getavailablecourses', {
@@ -11,14 +18,52 @@ export default function CourseList( { username } ) {
       })
           .then(response => response.json())
           .then(data => {
-              console.log(data);
-              let courseCount = data.length;
-              for (let i = 0; i < courseCount; i++) {
-                courses.push(data[i])
-              };
+            setCourses([]);
+            let courseCount = data.length;
+            let coursesList = [];
+            for (let i = 0; i < courseCount; i++) {
+              coursesList.push(data[i]);
+            };
+            setCourses(coursesList);
           })
     }
-  });
+    else {
+      fetch('/api/getenrolledcourses/' + username, {
+        method: 'GET'
+      })
+          .then(response => response.json())
+          .then(data => {
+            setCourses([]);
+            let courseCount = data.length;
+            let coursesList = [];
+            for (let i = 0; i < courseCount; i++) {
+              coursesList.push(data[i]);
+            };
+            setCourses(coursesList);
+          })
+    }
+  }, []);
+
+  const enrollHandler = courseName => {
+    setChosenCourse(courseName);
+    setEnrollAlert(true);
+  }
+
+  const enrollmentHandler = (courseName) => {
+    setEnrollAlert(false);
+    username = localStorage.getItem("username");
+    fetch('/api/enrollcourse/' + username + '&' + courseName, {
+      method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data['success']);
+          if (data['success']) {
+            history.push('/enrolledcourses');
+          }
+          window.location.reload();
+        })
+  }
 
   return (
     <div className="flex flex-col">
@@ -76,13 +121,13 @@ export default function CourseList( { username } ) {
                     <td className="px-6 py-4 whitespace-nowrap"> {course.professor} </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
                       <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
-                        {course.student.length}
+                        {course.students.length}
                       </span>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <a href="#" className="px-5 py-2 text-green-600 hover:text-green-900 rounded-md bg-green-200">
+                      <button id={course.name} onClick={e => enrollHandler(e.target.id)} className="px-5 py-2 text-green-600 hover:text-green-900 rounded-md bg-green-200 font-semibold">
                         Enroll
-                      </a>
+                      </button>
                     </td>
                   </tr>
                 ))}
@@ -91,6 +136,82 @@ export default function CourseList( { username } ) {
           </div>
         </div>
       </div>
+
+      <Transition.Root show={enrollAlert} as={Fragment}>
+        <Dialog
+          as="div"
+          static
+          className="fixed z-10 inset-0 overflow-y-auto"
+          initialFocus={cancelButtonRef}
+          open={enrollAlert}
+          onClose={setEnrollAlert}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                        Enroll
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Are you sure you want to enroll in<br/><span className="text-indigo-900 font-semibold">{chosenCourse}</span>?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => enrollmentHandler(chosenCourse)}
+                  >
+                    Confirm
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setEnrollAlert(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </div>
   )
 }
