dfde05f09d32e81fedba694e89afbafa1f905150
Shashanka Venkatesh <shashankavenkatesh127@gmail.com>
2021-04-20 11:56:34 +0530

    Major API refactoring

--- API/app.py
+++ API/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect, url_for
-from pymongo import MongoClient
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
+from pymongo import MongoClient
 from datetime import datetime
 import json
 
@@ -20,97 +20,145 @@ courses=db.get_collection('courses')
 forum=db.get_collection('forum')
 
 
-# -------------------------------------------------- #
-# ---------------- Defining the API ---------------- #
-# -------------------------------------------------- #
+# ---------------------------------------------------------------------------------------------------- #
+# ----------------------------------------- Defining the API ----------------------------------------- #
+# ---------------------------------------------------------------------------------------------------- #
 
-# --- Login & Register ----------------------------- #
+# --- Student Account CRUD Operations ----------------------------- #
 
-# Authenticate Student Login
-@app.route('/api/student-login/<username>&<password>/')
-def login(username, password):
-    success = student_records.count_documents({'username': username, 'password': password}) == 1
-    return ({"success": success})
+# Create a new student Account
+@app.route('/api/student-register', methods=['POST'])
+def create_student_account():
+    firstName = request.args['firstName']
+    lastName = request.args['lastName']
+    photoURL = request.args['photoURL']
+    username = request.args['username']
+    password = request.args['password']
 
-# Create a new Student Account
-@app.route('/api/student-register/<firstname>&<lastname>&<photourl>&<username>&<password>')
-def register(firstname,lastname,photourl,username, password):
     if(student_records.count_documents({'username': username}) == 1):
-	    return ({"success": False})
-    student_records.insert({'firstname': firstname, 'lastname': lastname, 'username': username, 'password': password, 'photourl': photourl})
+        return ({"success": False})
+
+    student_records.insert({'firstname': firstName, 'lastname': lastName, 'username': username, 'password': password, 'photoURL': photoURL})
     return ({"success": True})
 
 # Check if a student's username exists
-@app.route('/api/check-student-username/<username>/')
-def check_student_username(username):
+@app.route('/api/check-student-username', methods=['GET'])
+def check_student_username():
+    username = request.args['username']
+
     success = student_records.count_documents({'username': username}) == 1
     return ({"success": success})
 
-# Authenticate Professor Login
-@app.route('/api/professor-login/<username>&<password>/')
-def loginp(username, password):
-    success = professor_records.count_documents({'username': username, 'password': password}) == 1
+# Authenticate student Login
+@app.route('/api/student-login', methods=['GET'])
+def authenticate_student_login():
+    username = request.args['username']
+    password = request.args['password']
+    
+    success = student_records.count_documents({'username': username, 'password': password}) == 1
     return ({"success": success})
 
-# Create a new Professor Account
-@app.route('/api/professor-register/<firstname>&<lastname>&<photourl>&<username>&<password>')
-def registerp(firstname,lastname,photourl,username, password):
+# Delete a student Account
+@app.route('/api/student-delete', methods=['DELETE'])
+def delete_student_account():
+    username = request.args['username']
+
+    if(student_records.count_documents({'username': username}) == 1):
+        student_records.delete_one({'username': username})
+        return ({"success": True})
+    
+    return ({"success": False})
+
+# --- Professor Account CRUD Operations --------------------------- #
+
+# Create a new professor Account
+@app.route('/api/professor-register', methods=['POST'])
+def create_professor_account():
+    firstName = request.args['firstName']
+    lastName = request.args['lastName']
+    photoURL = request.args['photoURL']
+    username = request.args['username']
+    password = request.args['password']
+
     if(professor_records.count_documents({'username': username}) == 1):
-	    return ({"success": False})
-    professor_records.insert({'firstname': firstname, 'lastname': lastname, 'username': username, 'password': password, 'photourl': photourl})
+        return ({"success": False})
+
+    professor_records.insert({'firstname': firstName, 'lastname': lastName, 'username': username, 'password': password, 'photoURL': photoURL})
     return ({"success": True})
 
 # Check if a professor's username exists
-@app.route('/api/check-profesor-username/<username>/')
-def check_professor_username(username):
+@app.route('/api/check-professor-username', methods=['GET'])
+def check_professor_username():
+    username = request.args['username']
+
     success = professor_records.count_documents({'username': username}) == 1
     return ({"success": success})
 
+# Authenticate professor Login
+@app.route('/api/professor-login', methods=['GET'])
+def authenticate_professor_login():
+    username = request.args['username']
+    password = request.args['password']
+    
+    success = professor_records.count_documents({'username': username, 'password': password}) == 1
+    return ({"success": success})
+
+# Delete a professor Account
+@app.route('/api/professor-delete', methods=['DELETE'])
+def delete_professor_account():
+    username = request.args['username']
+
+    if(professor_records.count_documents({'username': username}) == 1):
+        professor_records.delete_one({'username': username})
+        return ({"success": True})
+    
+    return ({"success": False})
+
 # --- Profiles ------------------------------------- #
 
-@app.route('/api/student-profile/<username>')
+@app.route('/api/student-profile?<username>')
 def studentprofile(username):
     student_json = []
     if student_records.find({}):
         for student in student_records.find({"username":username}):
-            student_json.append({"firstname": student['firstname'], "lastname": student['lastname'], "username": student['username'], "password": student['password'],"photourl":student['photourl']})
+            student_json.append({"firstname": student['firstname'], "lastname": student['lastname'], "username": student['username'], "password": student['password'],"photoURL":student['photoURL']})
     return json.dumps(student_json)
 
-@app.route('/api/professor-profile/<username>')
+@app.route('/api/professor-profile?<username>')
 def professorprofile(username):
     professor_json = []
     if professor_records.find({}):
         for professor in professor_records.find({"username":username}):
-            professor_json.append({"firstname": professor['firstname'], "lastname": professor['lastname'], "username": professor['username'], "password": professor['password'],"photourl":professor['photourl']})
+            professor_json.append({"firstname": professor['firstname'], "lastname": professor['lastname'], "username": professor['username'], "password": professor['password'],"photoURL":professor['photoURL']})
     return json.dumps(professor_json)
 
 # --- Course Creation & Deletion ------------------- #
 
-@app.route('/api/create-course/<professor>&<name>&<description>')
+@app.route('/api/create-course?<professor>&<name>&<description>')
 def createcourse(professor,name,description):
     courses.insert_one({"name":name, "description": description, "students": [], "professor": professor})
     return ({"success": True})
 
-@app.route('/api/delete-course/<name>')
+@app.route('/api/delete-course?<name>')
 def deletecourse(name):
     courses.delete_one({"name": name})
     return ({"success": True})
 
 # --- Course Enrollment & Unenrollment ------------- #
 
-@app.route('/api/enroll-course/<username>&<name>')
+@app.route('/api/enroll-course?<username>&<name>')
 def enrollcourse(username, name):
    courses.update_one({"name":name}, {'$push': {"students": username}})
    return ({"success": True})
 
-@app.route('/api/unenroll-course/<username>&<name>')
+@app.route('/api/unenroll-course?<username>&<name>')
 def unenrollcourse(username,name):
     courses.update_one({"name":name}, {'$pull': {"students": username}})
     return ({"success": True})
 
 # --- Accessing Courses ---------------------------- #
 
-@app.route('/api/available-courses/<username>')
+@app.route('/api/available-courses?<username>')
 def getavailablecourses(username):
     courses_json = []
     enrolled_json= []
@@ -126,7 +174,7 @@ def getavailablecourses(username):
 
     return json.dumps(unenrolled_json)
 
-@app.route('/api/enrolled-courses/<username>')
+@app.route('/api/enrolled-courses?<username>')
 def getenrolledcourses(username):
     courses_json = []
 
@@ -136,7 +184,7 @@ def getenrolledcourses(username):
     
     return json.dumps(courses_json)
 
-@app.route('/api/created-courses/<name>')
+@app.route('/api/created-courses?<name>')
 def getcreatedcourses(name):
     courses_json = []
 
@@ -145,22 +193,22 @@ def getcreatedcourses(name):
             courses_json.append({"name": course['name'], "description": course['description'], "students": course['students'], "professor": course['professor']})
     
     return json.dumps(courses_json)
-	
-@app.route('/api/created-post/<details>&<username>&<name>')
+    
+@app.route('/api/created-post?<details>&<username>&<name>')
 def createpost(details,username,name):
    dateTimeObj = datetime.now()
    forum.insert_one({"name":name, "details": details, "username": username,"time":dateTimeObj})
    return ({"success": True})
-	
-@app.route('/api/view-post/<name>')
+    
+@app.route('/api/view-post?<name>')
 def viewallpost(name):
     forum_json = []
     if forum.find({}):
         for forums in forum.find({"name":name}):
             forum_json.append({"name":forums['name'], "details": forums['details'], "username": forums['username'],"time":forums['dateTimeObj']})
     return json.dumps(forum_json)
-	
-@app.route('/api/delete-post/<username>&<name>')
+    
+@app.route('/api/delete-post?<username>&<name>')
 def deletepost(username):
    forum.remove_one({"name":name, "username": username})
    return ({"success": True})
